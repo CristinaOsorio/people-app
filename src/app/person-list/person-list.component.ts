0d32161007261d48291937b6a90611b7d9ae4bb6
CristinaOsorio <macry_08@hotmail.com>
2024-09-14 02:00:46 -0600

    feat: add logic for crud in person-list with fake data

--- src/app/person-list/person-list.component.ts
+++ src/app/person-list/person-list.component.ts
@@ -1,17 +1,32 @@
 import { Component } from '@angular/core';
 import { Person } from '../core/interfaces/person.interface';
 import { FormsModule } from '@angular/forms';
+import { PersonFormComponent } from './components/person-form/person-form.component';
+import { ModalComponent } from '../shared/components/modal/modal.component';
+import { PersonDeleteComponent } from './components/person-delete/person-delete.component';
 
 @Component({
     selector: 'app-person-list',
     standalone: true,
-    imports: [FormsModule],
+    imports: [
+        FormsModule,
+        PersonFormComponent,
+        ModalComponent,
+        PersonDeleteComponent,
+    ],
     templateUrl: './person-list.component.html',
     styleUrl: './person-list.component.css',
 })
 export default class PersonListComponent {
+    titleModal = 'Person Form';
+    isOpenModalForm = false;
+    isOpenModalDelete = false;
+    showComponent: 'personForm' | 'personDelete' = 'personForm';
+    selectedPerson: Person | null = null;
+
     persons: Person[] = [
         {
+            id: 1,
             firstName: 'Juan',
             lastNamePaternal: 'Pérez',
             lastNameMaternal: 'González',
@@ -19,6 +34,7 @@ export default class PersonListComponent {
             phone: '555-1234',
         },
         {
+            id: 2,
             firstName: 'Ana',
             lastNamePaternal: 'López',
             lastNameMaternal: 'Martínez',
@@ -26,6 +42,7 @@ export default class PersonListComponent {
             phone: '555-5678',
         },
         {
+            id: 3,
             firstName: 'Luis',
             lastNamePaternal: 'Ramírez',
             lastNameMaternal: 'Hernández',
@@ -33,6 +50,7 @@ export default class PersonListComponent {
             phone: '555-9012',
         },
         {
+            id: 4,
             firstName: 'Carlos',
             lastNamePaternal: 'Mendoza',
             lastNameMaternal: 'Castillo',
@@ -40,6 +58,7 @@ export default class PersonListComponent {
             phone: '555-3456',
         },
         {
+            id: 5,
             firstName: 'Laura',
             lastNamePaternal: 'Garza',
             lastNameMaternal: 'Salinas',
@@ -47,6 +66,7 @@ export default class PersonListComponent {
             phone: '555-7890',
         },
         {
+            id: 6,
             firstName: 'Raúl',
             lastNamePaternal: 'Fernández',
             lastNameMaternal: 'Ortega',
@@ -54,10 +74,52 @@ export default class PersonListComponent {
             phone: '555-6543',
         },
     ];
+
     pageSize: number = 10;
     pageSizes: number[] = [5, 10, 20, 50, 100];
 
     onPageSizeChange(newPageSize: number): void {
         this.pageSize = newPageSize;
+    }
+
+    openModalForm(person?: Person) {
+        this.titleModal = person ? 'Editar persona' : 'Crear persona';
+        this.isOpenModalForm = !this.isOpenModalForm;
+        this.selectedPerson = person || null;
+    }
+
+    openModalDelete(person: Person) {
+        this.titleModal = 'Eliminar persona';
+        this.isOpenModalDelete = !this.isOpenModalDelete;
+        this.selectedPerson = person;
+    }
+
+    closeModal() {
+        this.isOpenModalDelete = false;
+        this.isOpenModalForm = false;
+        this.titleModal = '';
+        this.selectedPerson = null;
+    }
+
+    saveChanges(data: Person) {
+        this.selectedPerson ? this.updatePerson(data) : this.addPerson(data);
+        this.closeModal();
+    }
+
+    addPerson(data: Person) {
+        this.persons.push(data);
+    }
+
+    updatePerson(data: Person) {
+        const index = this.persons.findIndex((person) => person.id === data.id);
+        this.persons[index] = { ...data };
+    }
+
+    saveDelete(isDelete: boolean) {
+        if (isDelete) {
+            const { id } = this.selectedPerson || {};
+            this.persons = this.persons.filter((person) => person.id !== id);
+        }
+        this.closeModal();
     }
 }
